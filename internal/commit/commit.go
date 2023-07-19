787b227822ebfc30ea63da0945d23da392a938ef
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-07-19 22:43:11 +0800

    feat(commit): update commit.go and chatgpt.go files
    - Added import statement for the strings package in commit.go
    - Handled the error and exit status in the Commit function of commit.go
    - Generated a figure with the text go-commit in commit.go
    - Prompted for user confirmation before committing in commit.go
    - Executed the commit command using the generated commit message in commit.go
    - Handled the error and printed the result of the commit command in commit.go
    - Handled the case of empty commit confirmation input in commit.go
    - Created a new figure with the text go-commit in chatgpt.go
    - Set the OPENAI_API_KEY environment variable value in chatgpt.go
    - Added a check for the presence of the OPENAI_API_KEY environment variable in chatgpt.go
    - Generated a figure with instructions for setting up the OPENAI_API_KEY in chatgpt.go
    - Handled the case of missing OPENAI

--- internal/commit/commit.go
+++ internal/commit/commit.go
@@ -3,6 +3,7 @@ package commit
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/ak9024/go-commit/pkg/chatgpt"
 	"github.com/ak9024/go-commit/utils"
@@ -12,34 +13,54 @@ import (
 
 func Commit(cmd *cobra.Command, args []string) {
 	_cmd, err := utils.ExecCommand("git diff --staged")
-	if _cmd != "" {
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		generatedFigure := figure.NewFigure("go-commit", "", true)
-
-		// get `stdout` from `_cmd` and pass to chatgpt.GeneratedCommitMessageByChatGPT.
-		out, err := chatgpt.GeneratedCommitMessageByChatGPT(_cmd)
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		commitMessage := out.Choices[len(out.Choices)-1].Message.Content
-
-		generatedFigure.Print()
-		utils.PrintCommit(commitMessage)
-
-		// ask a quetion to make sure the commit suitable with the changes
-		commit := utils.StringPrompt("If ok, please confirm to commit [Y/n]? ")
-		if commit != "" && commit == "Y" {
-			gitCommit := fmt.Sprintf(`git commit -m "%s"`, commitMessage)
-			execCommit, _ := utils.ExecCommand(gitCommit)
-			fmt.Println(execCommit)
-		} else {
-			os.Exit(1)
-		}
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
 	}
+
+	generatedFigure := figure.NewFigure("go-commit", "", true)
+
+	if _cmd == "" {
+		generatedFigure.Print()
+		fmt.Println("")
+		fmt.Println("==========================================================================")
+		fmt.Println("Please doing git add <file> first or make sure git init in your repository")
+		os.Exit(0)
+	}
+
+	// get `stdout` from `_cmd` and pass to chatgpt.GeneratedCommitMessageByChatGPT.
+	out, err := chatgpt.GeneratedCommitMessageByChatGPT(_cmd)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(0)
+	}
+
+	commitMessage := out.Choices[len(out.Choices)-1].Message.Content
+
+	generatedFigure.Print()
+	utils.PrintCommit(commitMessage)
+
+	// ask a quetion to make sure the commit suitable with the changes
+	commit := utils.StringPrompt("If ok, please confirm to commit [Y/n]? ")
+
+	// handle case sensitive
+	if strings.EqualFold(commit, "y") {
+		// replace y to Y
+		commit = "Y"
+	}
+
+	if commit != "" && strings.EqualFold(commit, "Y") {
+		gitCommit := fmt.Sprintf(`git commit -m "%s"`, commitMessage)
+		execCommit, err := utils.ExecCommand(gitCommit)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(0)
+		}
+
+		fmt.Println(execCommit)
+		fmt.Println("Success....")
+	} else {
+		os.Exit(0)
+	}
+
 }
