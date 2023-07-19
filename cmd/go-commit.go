08b9b9990386105b2f3a6f6bcdcb49670a4b4db1
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-07-19 20:51:07 +0800

    chore(cmd): code cleanup
    - Removed unnecessary error handling in
    - Removed unused imports in
    - Updated dependencies in
    - Removed indirect dependencies in

--- cmd/go-commit.go
+++ cmd/go-commit.go
@@ -12,32 +12,34 @@ import (
 
 func RunCommand(cmd *cobra.Command, args []string) {
 	_cmd, err := utils.ExecCommand("git diff --staged")
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+	if _cmd != "" {
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
-	generatedFigure := figure.NewFigure("go-commit", "", true)
+		generatedFigure := figure.NewFigure("go-commit", "", true)
 
-	// get `stdout` from `_cmd` and pass to GeneratedCommitMessageByChatGPT.
-	out, err := chatgpt.GeneratedCommitMessageByChatGPT(_cmd)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
+		// get `stdout` from `_cmd` and pass to GeneratedCommitMessageByChatGPT.
+		out, err := chatgpt.GeneratedCommitMessageByChatGPT(_cmd)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
 
-	commitMessage := out.Choices[len(out.Choices)-1].Message.Content
+		commitMessage := out.Choices[len(out.Choices)-1].Message.Content
 
-	generatedFigure.Print()
-	utils.PrintCommit(commitMessage)
+		generatedFigure.Print()
+		utils.PrintCommit(commitMessage)
 
-	// ask a quetion to make sure the commit suitable with the changes
-	commit := utils.StringPrompt("If ok, please confirm to commit [Y/n]? ")
-	if commit != "" && commit == "Y" {
-		gitCommit := fmt.Sprintf(`git commit -m "%s"`, commitMessage)
-		execCommit, _ := utils.ExecCommand(gitCommit)
-		fmt.Println(execCommit)
-	} else {
-		os.Exit(1)
+		// ask a quetion to make sure the commit suitable with the changes
+		commit := utils.StringPrompt("If ok, please confirm to commit [Y/n]? ")
+		if commit != "" && commit == "Y" {
+			gitCommit := fmt.Sprintf(`git commit -m "%s"`, commitMessage)
+			execCommit, _ := utils.ExecCommand(gitCommit)
+			fmt.Println(execCommit)
+		} else {
+			os.Exit(1)
+		}
 	}
 }
