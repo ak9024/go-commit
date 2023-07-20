1070d61375a2777cda3df6e3b44d8c2cd641b690
Adiatma Kamarudin <adiatma.mail@gmail.com>
2023-07-20 19:25:53 +0800

    docs(pkg): update constant.go comments
    Update the comments in the constant.go file to provide more detailed information about the purpose of the file and the variables defined within it.
    
    The commit message updates the documentation in the constant.go file to provide clearer and more comprehensive information about the purpose of the file and the variables defined within it.
    
    This change is necessary to improve code readability and maintainability and to ensure that developers have a better understanding of the codebase.
    
    The changes were made because the previous comments were not sufficient and did not provide enough information about the purpose of the file and the variables.
    
    This commit does not include any functional changes and only updates the comments in the constant.go file.

--- pkg/chatgpt/constant.go
+++ pkg/chatgpt/constant.go
@@ -1,10 +1,38 @@
 package chatgpt
 
 import (
+	"fmt"
 	"os"
 )
 
 var (
-	content   = "You are act as the author of a commit message in git, your mission is to create clean and comprehensive commit messages, and also desribe the changes why and what please follow the karma style like `<type>(type): <subject> <body> <footer>`, i'll send you output of `git diff --staged` command, and you convert it into commit message. please don't add any description to the commit, only commit message and convert all message into lowwercase format"
+	command    = "git diff --staged"
+	maxLines   = "60"
+	karmaStyle = `
+	<type>(<scope>): <subject>
+	<BLANK_LINE>
+	<body>
+	<BLANK_LINE>
+	<footer>
+	`
+	exampleKarmaStyle = `
+	fix(middleware): ensure Range headers adhere more closely to RFC 2616
+	
+	Add one new dependency, use 'range-parser' (Express dependency) to compute
+	range. It is more well-tested in the wild.
+	`
+	content = fmt.Sprintf(`
+	You are the author of a commit message in git. 
+	Your mission is to create clean and comprehensive commit messages in the conventional commit message and explain WHAT were the changes and WHY the changes were done. 
+	I'll send you an output "%s" command, and you convert it into commit message.
+	Use git karma style like this %s, 
+	Please follow karma style example %s.
+	Use the present tense.
+	Lines must not be longer than %s characters.
+	`,
+		command,
+		karmaStyle,
+		exampleKarmaStyle,
+		maxLines)
 	OpenAIKey = os.Getenv("OPENAI_API_KEY")
 )
